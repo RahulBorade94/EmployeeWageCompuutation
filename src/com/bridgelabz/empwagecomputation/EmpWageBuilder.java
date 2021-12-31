35a3164afe33c4448cf294ff9a83153d3462c8cd
RahulBorade94 <boraderahul94@gmail.com>
2021-12-31 21:30:32 +0530

    MODIFIELD Calculating Employee Wages

--- src/com/bridgelabz/empwagecomputation/EmpWageBuilder.java
+++ src/com/bridgelabz/empwagecomputation/EmpWageBuilder.java
@@ -2,26 +2,28 @@ package com.bridgelabz.empwagecomputation;
 
 public class EmpWageBuilder {
 
-    public static int IS_FULL_TIME =1;
-    public static int IS_PART_TIME =2;
-    public static int EMP_RATE_PER_HOUR =20;
+    public static final int IS_FULL_TIME =2;
+    public static final int IS_PART_TIME =1;
+    public static final int EMP_RATE_PER_HOUR =20;
     public static void main(String[] args) {
 
         int empWage=0;
         int empHrs=0;
 
         int empCheck = (int) Math.floor(Math.random() * 10) %3;
-        if ( empCheck == IS_FULL_TIME)
+        switch (empCheck){
+            case IS_PART_TIME:
             empHrs =4;
-        else if (empCheck == IS_PART_TIME)
+            break;
+            case IS_FULL_TIME:
             empHrs =8;
-        else
-            empHrs =0;
+            break;
+            default :
+                empHrs =0;
+        }
 
         empWage = empHrs * EMP_RATE_PER_HOUR;
         System.out.println("Emp WAGE :"+ empWage);
-
-
 
     }
 
