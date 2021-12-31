65ecf265b15b93ff7d0bf1b84e71ae732876b680
RahulBorade94 <boraderahul94@gmail.com>
2021-12-31 15:44:07 +0530

    ADDED :Employee Wage Calculation

---
+++ src/com/bridgelabz/empwagecomputation/EmpWageBuilder.java
@@ -0,0 +1,16 @@
+package com.bridgelabz.empwagecomputation;
+
+public class EmpWageBuilder {
+    public static void main(String[] args) {
+        int IS_PRESENT =1;
+        int empCheck = (int) Math.floor(Math.random() * 10) %2;
+        if ( empCheck == IS_PRESENT)
+            System.out.println("Employee is Present");
+        else
+            System.out.println("Employee is Absent");
+
+
+
+    }
+
+}
