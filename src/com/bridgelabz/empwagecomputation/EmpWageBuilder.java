1c6d493ff239bc5f83833511232359c167a45019
RahulBorade94 <boraderahul94@gmail.com>
2021-12-31 19:49:33 +0530

    MODIFIELD: Adding part time employee

--- src/com/bridgelabz/empwagecomputation/EmpWageBuilder.java
+++ src/com/bridgelabz/empwagecomputation/EmpWageBuilder.java
@@ -3,19 +3,22 @@ package com.bridgelabz.empwagecomputation;
 public class EmpWageBuilder {
 
     public static int IS_FULL_TIME =1;
-    public static int EMP_PER_HOUR =20;
+    public static int IS_PART_TIME =2;
+    public static int EMP_RATE_PER_HOUR =20;
     public static void main(String[] args) {
 
         int empWage=0;
         int empHrs=0;
 
-        int empCheck = (int) Math.floor(Math.random() * 10) %2;
+        int empCheck = (int) Math.floor(Math.random() * 10) %3;
         if ( empCheck == IS_FULL_TIME)
+            empHrs =4;
+        else if (empCheck == IS_PART_TIME)
             empHrs =8;
         else
             empHrs =0;
 
-        empWage = empHrs * EMP_PER_HOUR;
+        empWage = empHrs * EMP_RATE_PER_HOUR;
         System.out.println("Emp WAGE :"+ empWage);
 
 
